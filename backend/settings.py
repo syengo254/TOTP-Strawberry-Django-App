3f6f0066fe862f53e056c63d151c044a8403675d
David Syengo <david.syengo019@gmail.com>
2022-10-30 19:40:18 +0300

    fix: support for dokku/heroku deployment

--- backend/settings.py
+++ backend/settings.py
@@ -91,15 +91,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
