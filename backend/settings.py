1ea5bb6151180fe6c7eaa1074eea4148de6aa071
David Syengo <david.syengo019@gmail.com>
2022-10-30 19:50:43 +0300

    fix: support for dokku/heroku deployment

--- backend/settings.py
+++ backend/settings.py
@@ -92,6 +92,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES["default"]["ENGINE"] = 'django.db.backends.postgresql'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
